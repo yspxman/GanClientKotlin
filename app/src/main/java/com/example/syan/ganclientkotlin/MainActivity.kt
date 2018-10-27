81b5b36b3cb77c1112c306d6d733400ad118bd74
Unknown <yspxman@gmail.com>
2018-10-27 23:51:59 +1100

    add layouts

--- app/src/main/java/com/example/syan/ganclientkotlin/MainActivity.kt
+++ app/src/main/java/com/example/syan/ganclientkotlin/MainActivity.kt
@@ -2,12 +2,13 @@ package com.example.syan.ganclientkotlin
 
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
 import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
@@ -26,12 +27,30 @@ class MainActivity : AppCompatActivity() {
         false
     }
 
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        val tt = findViewById(R.id.message) as TextView
+        val t2 = findViewById<TextView>(R.id.message)
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+
+    fun changeFragment(f: Fragment, clearnStack: Boolean =false){
+        val ft  = supportFragmentManager.beginTransaction();
+        if (clearnStack){
+
+        }
+
+        ft.setCustomAnimations(R.anim.abc_fade_in, R.anim.abc_fade_out, R.anim.abc_popup_enter, R.anim.abc_popup_exit);
+        //ft.replace(R.id.activi)
+        ft.addToBackStack(null);
+        ft.commit();
+    }
+
+    fun clearBackStack(){
+        val fm = supportFragmentManager;
+        if (fm.backStackEntryCount > 0){
+            fm.popBackStack(fm.getBackStackEntryAt(0).id, FragmentManager.POP_BACK_STACK_INCLUSIVE)
+        }
     }
 }
