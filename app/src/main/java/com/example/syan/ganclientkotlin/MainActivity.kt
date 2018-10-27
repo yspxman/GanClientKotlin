d9afef37e135c79a6e334822e4e00374674dd23e
Unknown <yspxman@gmail.com>
2018-10-28 00:18:17 +1100

    can run

--- app/src/main/java/com/example/syan/ganclientkotlin/MainActivity.kt
+++ app/src/main/java/com/example/syan/ganclientkotlin/MainActivity.kt
@@ -1,48 +1,30 @@
 package com.example.syan.ganclientkotlin
 
 import android.os.Bundle
-import android.support.design.widget.BottomNavigationView
+
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v7.app.AppCompatActivity
-import android.widget.TextView
+import android.widget.Toolbar
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_home -> {
-                message.setText(R.string.title_home)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
-                return@OnNavigationItemSelectedListener true
-            }
-        }
-        false
-    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val tt = findViewById(R.id.message) as TextView
-        val t2 = findViewById<TextView>(R.id.message)
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        findViewById<Toolbar>(R.id.toolbar)
+        //setSupportActionBar(toolbar)
     }
 
     fun changeFragment(f: Fragment, clearnStack: Boolean =false){
         val ft  = supportFragmentManager.beginTransaction();
         if (clearnStack){
-
+            clearBackStack()
         }
-
         ft.setCustomAnimations(R.anim.abc_fade_in, R.anim.abc_fade_out, R.anim.abc_popup_enter, R.anim.abc_popup_exit);
-        //ft.replace(R.id.activi)
+        ft.replace(R.id.activity_base_content, f)
         ft.addToBackStack(null);
         ft.commit();
     }
@@ -51,6 +33,16 @@ class MainActivity : AppCompatActivity() {
         val fm = supportFragmentManager;
         if (fm.backStackEntryCount > 0){
             fm.popBackStack(fm.getBackStackEntryAt(0).id, FragmentManager.POP_BACK_STACK_INCLUSIVE)
+        }
+    }
+
+    override fun onBackPressed() {
+        val fm =supportFragmentManager
+        if (fm.backStackEntryCount > 1){
+            fm.popBackStack()
+        }
+        else {
+            finish()
         }
     }
 }
